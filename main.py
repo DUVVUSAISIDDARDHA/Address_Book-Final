d2a0afb706d952e8964970c7d52ee0ce7976ca2f
DUVVUSAISIDDARDHA <ds8529@srmist.edu.in>
2025-07-10 11:23:20 +0530

    Getting contact details of a person

---
+++ main.py
@@ -0,0 +1,5 @@
+from contact import Contact
+
+contact_1 = Contact("Sai", "Siddu", "123 Street", "Chennai", "Tamil Nadu", "603203", "7093183272", "sai@example.com")
+# USE CASE 1 
+print(contact_1.display())
