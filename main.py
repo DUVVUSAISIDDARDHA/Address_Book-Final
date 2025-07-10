b552bbe8d1122f9864193bb8214bd9b367ea7582
DUVVUSAISIDDARDHA <ds8529@srmist.edu.in>
2025-07-10 12:04:45 +0530

    Adding multiple contacts

--- main.py
+++ main.py
@@ -1,35 +1,29 @@
 from contact import Contact
 from address_book import AddressBook
 
-
-
-contact1 = Contact(
-    first_name="Sai",
-    last_name="Siddu",
-    address="123 MG Road",
-    city="Hyderabad",
-    state="Telangana",
-    zip_code="500001",
-    phone="9876543210",
-    email="sai@example.com"
-)
-
-
-contact2 = Contact(
-    first_name="Anu",
-    last_name="Kumar",
-    address="456 Banjara Hills",
-    city="Hyderabad",
-    state="Telangana",
-    zip_code="500034",
-    phone="9123456780",
-    email="anu@example.com"
-)
-
 address_book = AddressBook()
-address_book.add_contact(contact1)
-address_book.add_contact(contact2)
 
-address_book.edit_contact("Anu")
-address_book.delete_contact("Anu")
+while True:
+    first_name = input("First Name: ")
+    last_name = input("Last Name: ")
+    address = input("Address: ")
+    city = input("City: ")
+    state = input("State: ")
+    zip_code = input("Zip Code: ")
+    phone = input("Phone: ")
+    email = input("Email: ")
+
+    contact = Contact(first_name, last_name, address, city, state, zip_code, phone, email)
+    address_book.add_contact(contact)
+
+    cont = input("Add another contact? (y/n): ").lower()
+    if cont != "y":
+        break
+
+edit_name = input("Enter name to edit: ")
+address_book.edit_contact(edit_name)
+
+delete_name = input("Enter name to delete: ")
+address_book.delete_contact(delete_name)
+
 address_book.display_all_contacts()
